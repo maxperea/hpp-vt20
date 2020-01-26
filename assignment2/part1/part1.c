912c75cbc11e51f082bcb7ae0f699368664a13b8
Max Perea <maxpereaduring@gmail.com>
2020-01-26 19:58:02 +0100

    start part2

---
+++ assignment2/part1/part1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int factorial(int n){
+  if (n <= 0)
+    return 1;
+  return n * factorial(n-1);
+}
+
+int over(int n, int k){
+  return factorial(n)/(factorial(k)*factorial(n-k));
+}
+
+int main(int argc, char** argv) {
+  printf("Enter amount of rows: ");
+  int rows;
+  scanf(" %i", &rows);
+  for (int n = 0; n < rows; n++){
+    for (int k = 0; k <= n; k++){
+      int num = over(n,k);
+      printf("%i ", num);
+    }
+    printf("\n");
+  }
+  return 0;
+}
