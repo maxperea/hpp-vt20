e87d0dd9837331fec5b3da606e34f4d16c615061
asdfelw <22148015+asdfelw@users.noreply.github.com>
2020-01-27 10:53:12 +0100

    Part2 klar-ish

--- assignment2/part2/part2.c
+++ assignment2/part2/part2.c
@@ -2,8 +2,29 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+int main() {
 
-int main(int argc, char** argv) {
+  FILE *fp = fopen("little_bin_file", "r");
 
+  if(fp == NULL)
+    {
+      perror("Error opening file!\n");
+      exit(0);
+    }
+
+  int i;
+  double d;
+  char c;
+  float f;
+
+  fread(&i, 1, sizeof(int), fp);
+  fread(&d, 1, sizeof(double), fp);
+  fread(&c, 1, sizeof(char), fp);
+  fread(&f, 1, sizeof(float), fp);
+
+  printf("%d\n%f\n%c\n%f\n", i,d,c,f);
+
+  fclose(fp);
+  
   return 0;
 }
