80a695e483142bd4c1fc4a79496054693320d355
Max Perea <maxpereaduring@gmail.com>
2020-02-10 12:35:25 +0100

    fixade komplettering, nu fungerande pascal

--- assignment2/part1/part1.c
+++ assignment2/part1/part1.c
@@ -2,15 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int factorial(int n){
-  if (n <= 0)
-    return 1;
-  return n * factorial(n-1);
-}
 
-int over(int n, int k){
-  return factorial(n)/(factorial(k)*factorial(n-k));
-}
 
 int main(int argc, char** argv) {
 
@@ -20,12 +12,20 @@ int main(int argc, char** argv) {
       return 0;
     }
 
-  // Assuming argument is int
   int rows = atoi(argv[1]);
-  for (int n = 0; n < rows; n++){
+  int arr[rows][rows];
+  arr[0][0] = 1;
+  printf("%i\n", arr[0][0]);
+
+  for (int n = 1; n < rows; n++){
     for (int k = 0; k <= n; k++){
-      int num = over(n,k);
-      printf("%i ", num);
+      if (k == 0 || k == n){
+        arr[n][k] = 1;
+        printf("%i ", arr[n][k]);
+      } else {
+        arr[n][k] = arr[n-1][k-1] + arr[n-1][k];
+        printf("%i ", arr[n][k]);
+      }
     }
     printf("\n");
   }
